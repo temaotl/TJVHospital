29f405282efa9db8ff5d743341fec176a76602b7
Artem Otliaguzov <temaotl0992@gmail.com>
2024-02-07 22:14:07 +0100

    make crud service for Card

--- hospital/src/main/java/org/example/hospital/business/CardService.java
+++ hospital/src/main/java/org/example/hospital/business/CardService.java
@@ -1,4 +1,19 @@
 package org.example.hospital.business;
 
-public class CardService {
+import org.example.hospital.data.dto.CardDto;
+import org.example.hospital.data.dto.convertes.toDto.CardToDto;
+import org.example.hospital.data.entity.Card;
+import org.example.hospital.data.repository.CardRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+
+@Service
+public class CardService extends  AbstractCrudService<CardDto,Long, Card, CardRepository > {
+
+    protected CardService(CardToDto toDtoConverter, CardRepository repository, ModelMapper modelMapper) {
+        super(toDtoConverter, repository, modelMapper);
+    }
 }
